604e8957ce013508e2eb612c992e0377dfd2934f
hopfmonoid <71606700+hopfmonoid@users.noreply.github.com>
2020-09-21 05:32:29 -0300

    Update assign.cc

--- assign.cc
+++ assign.cc
@@ -1,21 +1,7 @@
 // Copyright (C) 2015-2020 Bhalchandra Digambar Thatte
 
 // This file is a part of the project ASSIGN - a program to allocate disciplinas
-// (subjects) to professors
-
-// Author: Bhalchandra Digambar Thatte
-
-// ASSIGN is free software: you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or (at your option) any later
-// version.
-
-// ASSIGN is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-// A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License along with
-// this program.  If not, see <https://www.gnu.org/licenses/>.
+// (subjects) to professors. ASSIGN project is released under the GPL V3 license.
 
 // Some problems to be solved
 
